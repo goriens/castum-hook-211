6b8a0eef552367962e3cea322cd83ce283330eee
goriens <sohaibgoriens@gmail.com>
2022-07-06 00:39:18 +0530

    update search

--- P2/search-bar-211/src/App.jsx
+++ P2/search-bar-211/src/App.jsx
@@ -1,7 +1,39 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { SearchBar } from "./Components/SearchBar";
+import countries from "./Utils/countries";
 
 function App() {
-  return <div className="App">Search Bar</div>;
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    if (query === "") {
+      setSuggestions([]);
+    } else {
+      let newListOfSuggestions = countries
+        .filter((item) =>
+          item.country.toLowerCase().indexOf(query) !== -1 ? true : false
+        )
+        .map((item) => item.country);
+      setSuggestions(newListOfSuggestions);
+    }
+    setTimeout(() => setLoading(false), 500);
+  }, [query]);
+
+  return (
+    <div className="App">
+      <h1>Search bar</h1>
+      <SearchBar
+        loading={loading}
+        setLoading={setLoading}
+        suggestions={suggestions}
+        query={query}
+        onChange={(val) => setQuery(val)}
+      />
+    </div>
+  );
 }
 
 export default App;
